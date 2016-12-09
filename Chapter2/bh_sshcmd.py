b916745d96cc11415a9f50ddd89a79772e5bfed5
ioef <iosifidise@gmail.com>
2016-12-09 21:19:49 +0200

    ssh client

---
+++ Chapter2/bh_sshcmd.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import getpass
+import sys
+import paramiko
+import subprocess
+
+def ssh_command(ip, port, user, passwd, command):
+
+	client = paramiko.SSHClient()
+
+	client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	client.connect(ip, port, username=user, password=passwd)
+     
+	ssh_session = client.get_transport().open_session()
+	if ssh_session.active:
+		ssh_session.exec_command(command)
+		print ssh_session.recv(1024)
+	return
+
+
+
+def main():
+	
+	if len(sys.argv[1:]) != 3:
+		print "Usage: ./ssh_client.py server port username"
+		sys.exit(0)
+
+	server   = sys.argv[1]
+	port     = int(sys.argv[2])
+	username = sys.argv[3]
+	
+	password = getpass.getpass("Password:")
+	ssh_command(server, port,  username, password, 'id')
+
+if __name__=="__main__":
+	main()
+
